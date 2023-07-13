52a9b5002f95e3ab85486061f7975dce3f8c26d2
서재현 <ffgihh@gmail.com>
2023-07-13 14:18:40 +0900

    feat: index.html 수정

---
+++ src/main/java/org/ex/blog/controller/MainViewController.java
@@ -0,0 +1,13 @@
+package org.ex.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
